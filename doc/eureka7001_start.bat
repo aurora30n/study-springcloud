396eea721d29767e7f31d18be32a5db85b480de5
wangjiguang <wangjiguang@tusvn.com>
2020-04-28 16:43:26 +0800

    eureka

---
+++ doc/eureka7001_start.bat
@@ -0,0 +1,9 @@
+title eureka7001
+
+echo 当前路径:%~dp0
+
+java -jar eurekaserver.jar --spring.profiles.active=eureka7001
+
+@echo off
+rem 窗口不自动关闭
+pause
---
+++ doc/eureka7002_start.bat
@@ -0,0 +1,9 @@
+title eureka7002
+
+echo 当前路径:%~dp0
+
+java -jar eurekaserver.jar --spring.profiles.active=eureka7002
+
+@echo off
+rem 窗口不自动关闭
+pause
